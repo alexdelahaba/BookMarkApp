fcf5e809749c5288e2476a5dadffbfe78c3692ef
Alexdelahaba <alexmdelahaba@hotmail.com>
2020-07-29 11:10:13 +0200

    Grouping of bookmarks added

--- src/app/pages/display/display.component.ts
+++ src/app/pages/display/display.component.ts
@@ -18,12 +18,32 @@ export class DisplayComponent {
   showData() {
     this.store.select('bookmarks').subscribe((resp) => {
       if (resp && resp.length > 0) {
-        this.bookmarks = resp.filter((item) => item.name.length > 0);
+        const arrayBookmarks = resp.filter((item) => item.name.length > 0);
+        this.bookmarks = this.groupBy(arrayBookmarks, 'group');
       }
     });
   }
 
   deleteBookmark(bookmark: BookMark) {
     this.store.dispatch(new Actions.DeleteBookmark(bookmark));
+  }
+
+  groupBy(collection: BookMark[], property: string) {
+    let i = 0;
+    let val;
+    let index;
+    const values = [];
+    const result = [];
+    for (; i < collection.length; i++) {
+      val = collection[i][property];
+      index = values.indexOf(val);
+      if (index > -1) {
+        result[index].push(collection[i]);
+      } else {
+        values.push(val);
+        result.push([collection[i]]);
+      }
+    }
+    return result;
   }
 }
